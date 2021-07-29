86ebcd00839d3913b62269679cf2029032748a1b
githong@kakao.com <githong@kakao.com>
2021-07-29 15:37:09 +0000

    first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import * as http from 'http';
+let port = process.env.PORT || 4001
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!!!');
+}).listen(port);
