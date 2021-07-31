4d5e309522c4ee75b8999161c7ca5a5b4ba280f3
githong@kakao.com <githong@kakao.com>
2021-07-31 14:22:54 +0000

    split urls

--- app/app.js
+++ app/app.js
@@ -9,6 +9,17 @@ import {myDate} from './src/js/date.js';
 let port = process.env.PORT || 4001
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(`Hello World!!!! ${myDate()}`);
+
+    console.log(`url : ${req.url} method : ${req.method}`);
+
+    if(req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(`Hello World!!!! ${myDate()}`);
+    } else if (req.method === 'GET' && req.url === '/test') {
+        res.statusCode = 200;
+        res.end('Good Job!');
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 }).listen(port);
